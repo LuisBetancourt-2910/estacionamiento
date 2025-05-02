692d437f2d56797f5123d36011384f8d02de6b5f
Luis Betancourt <joseluisgarciabeta@gmail.com>
2025-05-02 16:37:54 -0600

    Update connection.js
    Se agrega comentarios de los cambios que se deben realizar al ejecutar el proyecto

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,12 +1,12 @@
 import sql from 'mssql';
 
 const dbConfig = {
-    user: 'Luis Betancourt',
-    password: 'Contraseña',
-    server: 'LUIS-BETANCOURT',
-    database: 'Estacionamiento',
+    user: 'Luis Betancourt',                  // Cambia esto por tu usuario de SQL Server
+    password: 'Contraseña',                   // Cambia esto por tu contraseña de SQL Server
+    server: 'LUIS-BETANCOURT',                // Cambia esto por tu servidor de SQL Server
+    database: 'Estacionamiento',              //esta se queda como va
     options: {
-        encrypt: true,
+        encrypt: true,                       //si no deja conectar utiliza false
         trustServerCertificate: true,
     },
     port: 1433
